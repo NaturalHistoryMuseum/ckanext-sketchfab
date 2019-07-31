f942b0a8fd627cc1fdf22d3690fb4c1dcac7df67
Alice Butcher <alycejenni@gmail.com>
2019-07-31 15:39:53 +0100

    Fixes tests

--- ckanext/sketchfab/tests/test_sketchfab_view.py
+++ ckanext/sketchfab/tests/test_sketchfab_view.py
@@ -1,74 +1,73 @@
-
-#!/usr/bin/env python
+# !/usr/bin/env python
 # encoding: utf-8
 #
 # This file is part of ckanext-sketchfab
 # Created by the Natural History Museum in London, UK
 
-import pylons.config as config
-import paste.fixture
-
-import ckan.config.middleware as middleware
-import ckan.model as model
-import ckan.lib.helpers as h
-import ckan.lib.create_test_data as create_test_data
-import ckan.plugins as p
-import ckan.tests as tests
+from ckan import plugins
+from ckan.plugins import toolkit
+from ckan.tests import factories, helpers
 
 
-
-
-class TestSketchfabView(tests.WsgiAppCase):
+class TestSketchfabView(helpers.FunctionalTestBase):
     ''' '''
+
+    @classmethod
+    def _apply_config_changes(cls, cfg):
+        # for testing views the plugin has to be loaded like this
+        cfg['ckan.plugins'] = u'sketchfab'
+
     @classmethod
     def setup_class(cls):
         ''' '''
-        cls.config_templates = config[u'ckan.legacy_templates']
-        config[u'ckan.legacy_templates'] = u'false'
-        wsgiapp = middleware.make_app(config[u'global_conf'], **config)
-        cls.app = paste.fixture.TestApp(wsgiapp)
+        super(TestSketchfabView, cls).setup_class()
+        cls.app = helpers._get_test_app()
+        helpers.reset_db()
+        if not plugins.plugin_loaded(u'sketchfab'):
+            plugins.load(u'sketchfab')
 
-        p.load(u'sketchfab')
+    @classmethod
+    def teardown_class(cls):
+        ''''''
+        if plugins.plugin_loaded(u'sketchfab'):
+            plugins.unload(u'sketchfab')
 
-        create_test_data.CreateTestData.create()
+    def setup(self):
+        self.user = factories.Sysadmin()
+        self.package = factories.Dataset(user=self.user)
+        self.resource = factories.Resource(user=self.user, package_id=self.package[u'id'])
+        self.resource_view = factories.ResourceView(
+            resource_id=self.resource[u'id'],
+            view_type=u'sketchfab',
+            title=u'Image View',
+            description=u'A nice view',
+            width=200,
+            height=400,
+            model_url=u'https://sketchfab.com/models/f157e030b89b4682bcc6ea808533c823')
 
-        context = {u'model': model,
-                   u'session': model.Session,
-                   u'user': model.User.get(u'testsysadmin').name}
-
-        cls.package = model.Package.get(u'annakarenina')
-        cls.resource_id = cls.package.resources[1].id
-        cls.resource_view = {
-            u'resource_id': cls.resource_id,
-            u'view_type': u'sketchfab',
-            u'title': u'Image View',
-            u'description': u'A nice view',
-            u'width': 200,
-            u'height': 400,
-            u'model_url': u'https://sketchfab.com/models/f157e030b89b4682bcc6ea808533c823'
-        }
-        p.toolkit.get_action(u'resource_view_create')(
-            context, cls.resource_view)
+    def teardown(self):
+        helpers.reset_db()
 
     def test_model_url_is_shown(self):
         ''' '''
-        url = h.url_for(controller=u'package', action=u'resource_read',
-                        id=self.package.name, resource_id=self.resource_id)
+        url = toolkit.url_for('resource.read',
+                              id=self.package['name'], resource_id=self.resource['id'])
         result = self.app.get(url)
         assert self.resource_view[u'model_url'] in result
 
     def test_title_description_iframe_shown(self):
         ''' '''
-        url = h.url_for(controller=u'package', action=u'resource_read', id=self.package.name, resource_id=self.resource_id)
+        url = toolkit.url_for('resource.read',
+                              id=self.package['name'], resource_id=self.resource['id'])
         result = self.app.get(url)
         assert self.resource_view[u'title'] in result
         assert self.resource_view[u'description'] in result
-        assert u'iframe' in result.body
+        assert u'iframe' in result.unicode_body
 
     def test_iframe_attributes(self):
         ''' '''
-        url = h.url_for(controller=u'package', action=u'resource_read', id=self.package.name, resource_id=self.resource_id)
+        url = toolkit.url_for('resource.read',
+                              id=self.package['name'], resource_id=self.resource['id'])
         result = self.app.get(url)
         assert u'width="%s"' % self.resource_view[u'width'] in result
         assert u'height="%s"' % self.resource_view[u'height'] in result
-
