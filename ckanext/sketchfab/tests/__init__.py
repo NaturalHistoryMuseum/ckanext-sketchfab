bd400d45c9c6ac1334dc009b8d419d4f3e85e921
Alice Butcher <alycejenni@gmail.com>
2019-07-31 15:38:57 +0100

    init files should declare namespace

--- ckanext/sketchfab/tests/__init__.py
+++ ckanext/sketchfab/tests/__init__.py
@@ -1,20 +1,12 @@
-
 #!/usr/bin/env python
 # encoding: utf-8
 #
 # This file is part of ckanext-sketchfab
 # Created by the Natural History Museum in London, UK
 
-import sys
-import os
-
-
-def main():
-    ''' '''
-    pass
-
-
-if __name__ == u'__main__':
-    main()
-
-
+try:
+    import pkg_resources
+    pkg_resources.declare_namespace(__name__)
+except ImportError:
+    import pkgutil
+    __path__ = pkgutil.extend_path(__path__, __name__)
