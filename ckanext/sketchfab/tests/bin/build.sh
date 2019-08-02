b3b1b6137b6135545b1fb6094bad2adfd3044340
Alice Butcher <alycejenni@gmail.com>
2019-08-02 14:24:51 +0100

    Adding travis/build files

---
+++ ckanext/sketchfab/tests/bin/build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+HERE=$(pwd)
+
+sudo git clone --branch=2.8 https://github.com/ckan/ckan /ckan
+sudo chmod -R a+rw /ckan
+
+wget http://archive.apache.org/dist/lucene/solr/5.5.4/solr-5.5.4.tgz -O /tmp/solr.tgz
+sudo mkdir /solr
+sudo tar xzf /tmp/solr.tgz -C /solr
+sudo /solr/solr-5.5.4/bin/solr start
+sudo /solr/solr-5.5.4/bin/solr create_core -c ckan
+sudo cp /ckan/ckan/config/solr/schema.xml /solr/solr-5.5.4/server/solr/ckan/conf/
+sudo /solr/solr-5.5.4/bin/solr restart
+
+cd /ckan
+pip install --upgrade pip
+pip install -r requirement-setuptools.txt
+pip install -r requirements.txt
+pip install -r dev-requirements.txt
+python setup.py develop
+
+sudo -u postgres psql -c "CREATE USER ckan_default WITH PASSWORD 'pass';"
+sudo -u postgres psql -c 'CREATE DATABASE ckan_test WITH OWNER ckan_default;'
+
+paster db init -c /ckan/test-core.ini
+
+cd $HERE
+pip install -r requirements.txt
+pip install -r dev_requirements.txt
+pip install -e .
